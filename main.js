d86a8bd23f59070b8d811cf91bc24b6ca8c5cc38
Daniel <dansto13@yahoo.com>
2018-02-09 14:04:15 +0100

    Add characters remaining functionality

---
+++ main.js
@@ -0,0 +1,25 @@
+const MAX_TWEET_LENGTH = 140;
+
+new Vue({
+  el: "#twitterVue",
+  data: {
+    tweet: ""
+  },
+  computed: {
+    tweetIsOutOfRange: function() {
+      return (
+        this.charactersRemaining == MAX_TWEET_LENGTH ||
+        this.charactersRemaining < 0
+      );
+    },
+    charactersRemaining: function() {
+      return MAX_TWEET_LENGTH - this.tweet.length;
+    },
+    underTwentyMark: function() {
+      return this.charactersRemaining <= 20 && this.charactersRemaining > 10;
+    },
+    underTenMark: function() {
+      return this.charactersRemaining <= 10;
+    }
+  }
+});
