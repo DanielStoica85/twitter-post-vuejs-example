0becdc6efc4615ea0748389e3ab0444cb916f598
Daniel <dansto13@yahoo.com>
2018-02-09 14:54:24 +0100

    Move content in modal and add animation.

--- README.md
+++ README.md
@@ -16,7 +16,8 @@ Basic Vue.js component for composing tweets. Built for learning purposes by foll
 1. Disable the blue Tweet button until a user has entered at least one character in the textarea.
 2. As a user types, show the number of characters remaining (out of 140) in the tweet. If a user has entered more than 140 characters, disable the blue Tweet button.
 3. When composing a Tweet, the color of the “characters remaining” indicator should change to dark red when only twenty characters remain, and light red when ten or fewer remain.
-4. Allow users to attach a single photo to their tweet via a file picker dialog. When the photo has been uploaded, show it beneath the textarea, and allow users to delete the attachment by clicking the image. TBD
+4. Allow users to attach photos to their tweet via a file picker dialog. When the photo has been uploaded, show it beneath the textarea, and allow users to delete the attachment by clicking the image.
+5. CSS Animations
 
 
 
--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const MAX_TWEET_LENGTH = 140;
 new Vue({
   el: "#twitterVue",
   data: {
-    tweet: ""
+    tweet: "",
+    photos: []
   },
+
   computed: {
     tweetIsOutOfRange: function() {
       return (
@@ -20,6 +22,46 @@ new Vue({
     },
     underTenMark: function() {
       return this.charactersRemaining <= 10;
+    },
+    photoHasBeenUploaded: function() {
+      return this.photos.length > 0;
+    }
+  },
+
+  methods: {
+    triggerFileUpload: function() {
+      this.$refs.photoUpload.click();
+    },
+    handlePhotoUpload: function(e) {
+      var self = this;
+      var files = e.target.files;
+
+      for (let i = 0; i < files.length; i++) {
+        let reader = new FileReader();
+        reader.onloadend = function(evt) {
+          self.photos.push(evt.target.result);
+        };
+        reader.readAsDataURL(files[i]);
+      }
+    },
+    removePhoto: function(index) {
+      this.photos.splice(index, 1);
+    },
+    hideModal: function() {
+      this.modalShowing = false;
+    },
+    showModal: function() {
+      this.modalShowing = true;
+    },
+    modalEnter: function(el, done) {
+      Velocity(el, "fadeIn", {
+        duration: 300,
+        complete: done,
+        display: "flex"
+      });
+    },
+    modalLeave: function(el, done) {
+      Velocity(el, "fadeOut", { duration: 300, complete: done });
     }
   }
 });
